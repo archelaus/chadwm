b4b864292a7170abfea0e9184d365df3af20b831
archelaus <84364760+archelaus@users.noreply.github.com>
2024-01-18 11:19:21 +0530

    feat: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
 
-xrdb merge ~/.Xresources 
-xbacklight -set 10 &
-feh --bg-fill ~/Pictures/wall/gruv.png &
-xset r rate 200 50 &
-picom &
+setroot --restore
+# xmodmap ~/.Xmodmap
+xrdb merge ~/.config/x11/xresources
 
-dash ~/.config/chadwm/scripts/bar.sh &
-while type chadwm >/dev/null; do chadwm && continue || break; done
+dunst &
+greenclip daemon &
+sxhkd &
+
+xidlehook --not-when-fullscreen --detect-sleep --not-when-audio \
+	--timer 300 "xrandr --output eDP1 --brightness .1" "xrandr --output eDP1 --brightness 1" \
+	--timer 300 "xrandr --output eDP1 --brightness 1; i3lock-extra" "" \
+	--timer 300 "sudo systemctl suspend" "" &
+
+# for id in $(xinput list | grep "AT Translated Set 2 keyboard" | sed -E 's|.*=([0-9]+).*|\1|'); do
+# 	xinput float "$id"
+# done
+
+# https://gist.github.com/bumbummen99/fcab50dad5638399375693a70a8c17f4
+# xinput set-button-map 15 1 0 3 4 5 6 7
+
+firefox &
+kdeconnect-indicator &
+syncthing serve --no-browser &
+workrave &
+
+~/.local/share/virtualenvs/yt/bin/python ~/.local/src/youtube-local/server.py &
+# python -m http.server 7389 --directory ~/.local/src/_traichu/ &
+
+~/.local/src/chadwm/scripts/bar.sh &
+
+while type chadwm >/dev/null; do
+	chadwm && continue || break
+done
