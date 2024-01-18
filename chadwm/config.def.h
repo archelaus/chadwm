dcdc18d8c08f56f2be6599ca7faa5ecd993ba14a
archelaus <84364760+archelaus@users.noreply.github.com>
2024-01-18 10:50:49 +0530

    feat: change appearance, disable keybindings

--- chadwm/config.def.h
+++ chadwm/config.def.h
@@ -3,50 +3,63 @@
 #include <X11/XF86keysym.h>
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+/* static const unsigned int borderpx  = 0; */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
 static const unsigned int default_border = 0;   /* to switch back to default border after dynamic border resizing via keybinds */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 1;        /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails,display systray on the 1st monitor,False: display systray on last monitor*/
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the 1st monitor, False: display systray on last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int showtab            = showtab_auto;
 static const int toptab             = 1;        /* 0 means bottom tab */
-static const int floatbar           = 1;/* 1 means the bar will float(don't have padding),0 means the bar have padding */
+static const int floatbar           = 0;        /* 1 means the bar will float (don't have padding), 0 means the bar have padding */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 5;
-static const int vertpadbar         = 11;
+/* static const int vertpadbar         = 11; */
+static const int vertpadbar         = 8;
 static const int vertpadtab         = 35;
 static const int horizpadtabi       = 15;
 static const int horizpadtabo       = 15;
 static const int scalepreview       = 4;
-static const int tag_preview        = 0;        /* 1 means enable, 0 is off */
+static const int tag_preview        = 1;        /* 1 means enable, 0 is off */
 static const int colorfultag        = 1;        /* 0 means use SchemeSel for selected non vacant tag */
-static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
-static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
-static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
-static const char *light_up[] = {"/usr/bin/light", "-A", "5", NULL};
-static const char *light_down[] = {"/usr/bin/light", "-U", "5", NULL};
 static const int new_window_attach_on_end = 0; /*  1 means the new window will attach on the end; 0 means the new window will attach on the front,default is front */
+
+/* static const char *upvol[]      = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL }; */
+/* static const char *downvol[]    = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL }; */
+/* static const char *mutevol[]    = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL }; */
+/* static const char *light_up[]   = { "/usr/bin/light", "-A", "5", NULL}; */
+/* static const char *light_down[] = { "/usr/bin/light", "-U", "5", NULL}; */
+
 #define ICONSIZE 19   /* icon size */
 #define ICONSPACING 8 /* space between icon and title */
 
-static const char *fonts[]          = {"Iosevka:style:medium:size=12" ,"JetBrainsMono Nerd Font Mono:style:medium:size=19" };
+/* static const char *fonts[] = { */
+/*   "Iosevka:style:medium:size=12", */
+/*   "JetBrainsMono Nerd Font Mono:style:medium:size=19" */
+/* }; */
+
+static const char *fonts[] = {
+  "Iosevka:style:medium:size=12",
+  "Symbols Nerd Font:size=12"
+};
 
 // theme
-#include "themes/onedark.h"
+#include "themes/gruvchad.h"
 
 static const char *colors[][3]      = {
     /*                     fg       bg      border */
     [SchemeNorm]       = { gray3,   black,  gray2 },
-    [SchemeSel]        = { gray4,   blue,   blue  },
-    [SchemeTitle]      = { white,   black,  black  }, // active window title
+    /* [SchemeSel]        = { gray4,   blue,   blue  }, */
+    [SchemeSel]        = { gray4,   blue,   gray3 },
+    [SchemeTitle]      = { white,   black,  black }, // active window title
     [TabSel]           = { blue,    gray2,  black },
     [TabNorm]          = { gray3,   black,  black },
     [SchemeTag]        = { gray3,   black,  black },
@@ -62,13 +75,15 @@ static const char *colors[][3]      = {
 };
 
 /* tagging */
-static char *tags[] = {"", "", "", "", ""};
+/* static char *tags[] = {"", "", "", "", ""}; */
+static char *tags[] = {"", "", "", "󰙯", ""};
 
-static const char* eww[] = { "eww", "open" , "eww", NULL };
+/* static const char* eww[] = { "eww", "open" , "eww", NULL }; */
+static const char* rofi[] = { "launcher_t3", NULL };
 
 static const Launcher launchers[] = {
     /* command     name to display */
-    { eww,         "" },
+    { rofi,         "󰣭" },
 };
 
 static const int tagschemes[] = {
@@ -136,18 +151,18 @@ static const Layout layouts[] = {
 static const Key keys[] = {
     /* modifier                         key         function        argument */
 
-    // brightness and audio 
-    {0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol}},
-	{0,                       XF86XK_AudioMute, spawn, {.v = mutevol }},
-	{0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol}},
-	{0,				XF86XK_MonBrightnessUp,		spawn,	{.v = light_up}},
-	{0,				XF86XK_MonBrightnessDown,	spawn,	{.v = light_down}},
+    /* // brightness and audio */
+    /* {0,                    XF86XK_AudioLowerVolume , spawn, {.v = downvol}}   , */
+    /* {0,                    XF86XK_AudioMute        , spawn, {.v = mutevol }}  , */
+    /* {0,                    XF86XK_AudioRaiseVolume , spawn, {.v = upvol}}     , */
+    /* {0,                    XF86XK_MonBrightnessUp  , spawn, {.v = light_up}}  , */
+    /* {0,                    XF86XK_MonBrightnessDown, spawn, {.v = light_down}}, */
 
-    // screenshot fullscreen and cropped
-    {MODKEY|ControlMask,                XK_u,       spawn,
-        SHCMD("maim | xclip -selection clipboard -t image/png")},
-    {MODKEY,                            XK_u,       spawn,
-        SHCMD("maim --select | xclip -selection clipboard -t image/png")},
+    /* // screenshot fullscreen and cropped */
+    /* { MODKEY|ControlMask,                XK_u,       spawn, */
+    /*     SHCMD("maim | xclip -selection clipboard -t image/png")}, */
+    /* { MODKEY,                            XK_u,       spawn, */
+    /*     SHCMD("maim --select | xclip -selection clipboard -t image/png")}, */
 
     { MODKEY,                           XK_c,       spawn,          SHCMD("rofi -show drun") },
     { MODKEY,                           XK_Return,  spawn,            SHCMD("st")},
