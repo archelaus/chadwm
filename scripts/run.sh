830eb21914df9fafe878f7e76688ed2b39208214
archelaus <84364760+archelaus@users.noreply.github.com>
2024-04-06 20:38:25 +0530

    chore: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,33 +1,23 @@
 #!/bin/sh
 
-xmodmap ~/.Xmodmap
 setroot --restore
+setxkbmap -option caps:swapescape
 xrdb merge ~/.config/x11/xresources
 
 dunst &
 greenclip daemon &
-# sudo xremap ~/.config/xremap/config.yml &
 sxhkd &
 
 xidlehook --not-when-fullscreen --detect-sleep --not-when-audio \
 	--timer 300 "xrandr --output eDP1 --brightness .1" "xrandr --output eDP1 --brightness 1" \
 	--timer 300 "xrandr --output eDP1 --brightness 1; i3lock-extra" "" \
-	--timer 300 "sudo systemctl suspend" "" &
+	--timer 300 "systemctl suspend" "" &
 
-# for id in $(xinput list | grep "AT Translated Set 2 keyboard" | sed -E 's|.*=([0-9]+).*|\1|'); do
-#   xinput float "$id"
-# done
-
-# https://gist.github.com/bumbummen99/fcab50dad5638399375693a70a8c17f4
-# xinput set-button-map 15 1 0 3 4 5 6 7
-
-firefox &
+firefox -P default &
 kdeconnect-indicator &
 syncthing serve --no-browser &
 workrave &
 
-# python -m http.server 7389 --directory ~/.local/src/_traichu/ &
-echo ~/.cache/greenclip.history | entr -np monitor &
 ~/.local/share/virtualenvs/yt/bin/python ~/.local/src/youtube-local/server.py &
 
 ~/.local/src/chadwm/scripts/bar.sh &
