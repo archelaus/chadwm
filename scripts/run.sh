1a810923d60ff685248b33ab07601c40111d728b
archelaus <84364760+archelaus@users.noreply.github.com>
2024-04-18 18:40:31 +0530

    fix: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -8,29 +8,31 @@ _ka() {
 	killall "$@"
 }
 
-setxkbmap -option caps:swapescape
-xrdb merge ~/.config/x11/xresources
-
-# Run once and never kill
-_px sxhkd >/dev/null || sxhkd 2>&1 | tee ~/.cache/logs/autostart/sxhkd.log &
-_px greenclip >/dev/null || greenclip daemon 2>&1 | tee ~/.cache/logs/autostart/greenclip.log &
-
-# Kill all already running autostart programs
-_ka dunst >/dev/null
-_ka xidlehook >/dev/null
-
 # Set wallpaper
 setroot --restore
 
+setxkbmap -option caps:swapescape
+xrdb merge ~/.config/x11/xresources
+
 # Create a log directory for autostarted apps
-[ -d ~/.cache/logs ] || mkdir ~/.cache/logs
-[ -d ~/.cache/logs/autostart ] || mkdir ~/.cache/logs/autostart
+LOG_DIR=~/.cache/logs/autostart
+[ -d "$LOG_DIR" ] || mkdir -p "$LOG_DIR"
+
+# Run once and never kill
+_px greenclip >/dev/null || greenclip daemon 2>&1 &
+_px sxhkd >/dev/null || sxhkd 2>&1 | tee "$LOG_DIR/sxhkd.log" &
+
+# Kill all already running autostart programs
+_ka bar.sh >/dev/null
+_ka dunst >/dev/null
+_ka python >/dev/null
+_ka xidlehook >/dev/null
 
 # Start dunst and log
-dunst -verbosity warn 2>&1 | tee ~/.cache/logs/autostart/dunst.log &
+dunst -verbosity warn 2>&1 | tee "$LOG_DIR/dunst.log" &
 
 # Start xidlehook
-xidlehook --not-when-fullscreen --detect-sleep --not-when-audio \
+xidlehook --detect-sleep --not-when-audio --not-when-fullscreen \
 	--timer 300 "xrandr --output eDP1 --brightness .1" "xrandr --output eDP1 --brightness 1" \
 	--timer 300 "xrandr --output eDP1 --brightness 1; i3lock-extra" "" \
 	--timer 300 "systemctl suspend" "" &
@@ -42,7 +44,7 @@ workrave &
 
 ~/.local/share/virtualenvs/yt/bin/python ~/.local/src/youtube-local/server.py &
 
-~/.local/src/chadwm/scripts/bar.sh &
+~/.local/src/chadwm/scripts/bar.sh 2>&1 | tee "$LOG_DIR/dwm-bar.log" &
 
 while type chadwm >/dev/null; do
 	chadwm && continue || break
