0068cd141eb4cdcc1099e445536da52e1ba18a25
archelaus <84364760+archelaus@users.noreply.github.com>
2024-04-18 13:08:04 +0530

    feat: update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,13 +1,35 @@
 #!/bin/sh
 
-setroot --restore
+_px() {
+	pgrep -x "$@"
+}
+
+_ka() {
+	killall "$@"
+}
+
 setxkbmap -option caps:swapescape
 xrdb merge ~/.config/x11/xresources
 
-dunst &
-greenclip daemon &
-sxhkd &
+# Run once and never kill
+_px sxhkd >/dev/null || sxhkd 2>&1 | tee ~/.cache/logs/autostart/sxhkd.log &
+_px greenclip >/dev/null || greenclip daemon 2>&1 | tee ~/.cache/logs/autostart/greenclip.log &
 
+# Kill all already running autostart programs
+_ka dunst >/dev/null
+_ka xidlehook >/dev/null
+
+# Set wallpaper
+setroot --restore
+
+# Create a log directory for autostarted apps
+[ -d ~/.cache/logs ] || mkdir ~/.cache/logs
+[ -d ~/.cache/logs/autostart ] || mkdir ~/.cache/logs/autostart
+
+# Start dunst and log
+dunst -verbosity warn 2>&1 | tee ~/.cache/logs/autostart/dunst.log &
+
+# Start xidlehook
 xidlehook --not-when-fullscreen --detect-sleep --not-when-audio \
 	--timer 300 "xrandr --output eDP1 --brightness .1" "xrandr --output eDP1 --brightness 1" \
 	--timer 300 "xrandr --output eDP1 --brightness 1; i3lock-extra" "" \
